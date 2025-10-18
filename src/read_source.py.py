594dc6f10b3b0ae5c1a670780f95c15015bc4b58
Sid-ht <Sid-ht>
2025-10-18 18:00:50 +0000

    ingestion_code

---
+++ src/read_source.py.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.types import *
+import pyspark.sql.functions as F
+import pandas as pd
+
+def corrected_column_names(df):
+    new_columns = [c.strip().replace(' ', '_').lower() for c in df.columns]
+    for old, new in zip(df.columns, new_columns):
+        df = df.withColumnRenamed(old, new)
+    return df
+
+def read_orders(path: str) -> DataFrame:
+    df = spark.read.option("multiline", True).json(path)
+    new_df = corrected_column_names(df)
+    return new_df
+
+def read_products_csv(path: str) -> DataFrame:
+    df = spark.read.option("header", True).csv(path)
+    new_df = corrected_column_names(df)
+    return new_df
+
+def read_customers_xlsx_via_pandas(path: str):
+    pandas_df = pd.read_excel(path)
+    if 'phone' in pandas_df.columns:
+        pandas_df['phone'] = pandas_df['phone'].astype(str)
+    df = spark.createDataFrame(pandas_df)
+    new_df = corrected_column_names(df)
+    return new_df
