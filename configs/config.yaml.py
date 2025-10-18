0690f3d381d738347efa53e30a9b053a6653add9
Sid-ht <Sid-ht>
2025-10-18 20:27:06 +0000

    yaml change

---
+++ configs/config.yaml.py
@@ -0,0 +1,5 @@
+# Databricks notebook source
+paths:
+  orders: '/Volumes/sales_processing/bronze/source_datasets/Orders.json'
+  products: '/Volumes/sales_processing/bronze/source_datasets/Products.csv'
+  customers: '/Volumes/sales_processing/bronze/source_datasets/Customer.xlsx'
