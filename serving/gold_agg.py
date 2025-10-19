b9d8087c04ad06766aca2695513404f91a86d565
Sid-ht <Sid-ht>
2025-10-19 15:32:18 +0000

    changes to py file

---
+++ serving/gold_agg.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+from pyspark.sql import DataFrame
+import pyspark.sql.functions as F
+
+def agg_by_year(silver_df: DataFrame)-> DataFrame:
+    return silver_df.groupBy(F.col('year')).agg(F.sum('profit').alias('year_wise_profit'))
+
+def agg_by_category(silver_df: DataFrame, silver_df2: DataFrame )-> DataFrame:
+    return silver_df.join(silver_df2, on ='product_id').groupBy(F.col('category')).agg(F.sum('profit').alias('category_wise_profit'))
+
+def agg_by_sub_category(silver_df: DataFrame, silver_df2: DataFrame )-> DataFrame:
+    return silver_df.join(silver_df2, on ='product_id').groupBy(F.col('sub_category')).agg(F.sum('profit').alias('sub_category_wise_profit'))
+
+def agg_by_customer(silver_df: DataFrame, silver_df2: DataFrame )-> DataFrame:
+    return silver_df.join(silver_df2, on ='customer_id').groupBy(F.col('customer_id'), F.col('customer_name')).agg(F.sum('profit').alias('customer_wise_profit'))
